a2e8c4a84056cb140cbb9667828a7dd93e20913c
Quill Huntley <quillhuntley@Quills-MacBook-Pro.local>
2017-06-13 11:21:23 -0400

    Rename db

--- assignment/app.js
+++ assignment/app.js
@@ -1,7 +1,17 @@
 var app = require('../express');
 var mongoose = require('mongoose');
 mongoose.Promise = require('q').Promise;
-mongoose.createConnection('mongodb://localhost/webdev');
+var connectionString = 'mongodb://127.0.0.1:27017/webdev';
+
+if(process.env.MLAB_USERNAME) {
+    connectionString = process.env.MLAB_USERNAME + ":" +
+        process.env.MLAB_PASSWORD + "@" +
+        process.env.MLAB_HOST + ':' +
+        process.env.MLAB_PORT + '/' +
+        process.env.MLAB_APP_NAME;
+}
+
+mongoose.createConnection(connectionString);
 
 require('./services/user.service.server');
 require('./services/website.service.server');
