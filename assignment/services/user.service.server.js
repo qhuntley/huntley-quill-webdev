cc5a51af5372ce04cb78b513fb10363ad7aa7bc5
Quill Huntley <quillhuntley@Quills-MacBook-Pro.local>
2017-06-17 15:45:48 -0400

    Updated home

--- assignment/services/user.service.server.js
+++ assignment/services/user.service.server.js
@@ -17,7 +17,7 @@ app.get ('/auth/facebook', passport.authenticate('facebook', { scope : 'email' }
 
 app.get('/auth/facebook/callback',
     passport.authenticate('facebook', {
-        successRedirect: '/#/user',
+        successRedirect: '/assignment/#!/profile',
         failureRedirect: '/#/login'
     }));
 
